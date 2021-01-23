82590f3a938614a011ea6a7fb2f1d0a0be6207e1
Artsiom <artsiom_peshko@epam.com>
2021-01-24 01:14:31 +0300

    clean

--- src/main/java/com/apeshko/javaspring/dao/UserDao.java
+++
@@ -1,8 +0,0 @@
-package com.apeshko.javaspring.dao;
-
-import java.util.List;
-
-public interface UserDao<T> extends Dao<T> {
-    T getByEmail(String email);
-    List<T> getUsersByName(String name, int pageSize, int pageNum);
-}
